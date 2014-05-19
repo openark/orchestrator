fd0ffed19e467a0612bb9d1fb36379d6972c37e8
snoach <snoach@outbrain.com>
2014-05-19 10:58:43 +0300

    problems sidebar now collapsible

--- resources/public/js/instance-problems.js
+++ resources/public/js/instance-problems.js
@@ -2,18 +2,15 @@
 $(document).ready(function () {
     showLoader();
     $.get("/api/problems", function (instances) {
-        $.get("/api/maintenance",
-            function (maintenanceList) {
-        		normalizeInstances(instances, maintenanceList);
-                displayProblemInstances(instances);
-            }, "json");
-    }, "json");
+			normalizeInstances(instances, []);
+	        displayProblemInstances(instances);
+	    }, "json");
     function displayProblemInstances(instances) {
         hideLoader();
         
     	instances.forEach(function (instance) {
     		if (instance.hasProblem) {
-	    		$("#instance_problems").append('<div xmlns="http://www.w3.org/1999/xhtml" class="popover instance right" data-nodeid="'+instance.id+'"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content"></div></div>');
+	    		$("#instance_problems .panel-body").append('<div xmlns="http://www.w3.org/1999/xhtml" class="popover instance right" data-nodeid="'+instance.id+'"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content"></div></div>');
 	
 	    		var popoverElement = $("#instance_problems [data-nodeid='" + instance.id + "'].popover");
 	    		renderInstanceElement(popoverElement, instance, "problems");
