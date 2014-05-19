338a19f9be2a7f814807e5fb84ffca27d2579ec0
snoach <snoach@outbrain.com>
2014-05-19 13:51:52 +0300

    auto-hide problems panel when no problems shown

--- resources/public/js/instance-problems.js
+++ resources/public/js/instance-problems.js
@@ -13,14 +13,19 @@ $(document).ready(function () {
         }
         instances.sort(SortByProblemOrder);
 
+        var problemInstancesFound = false;
     	instances.forEach(function (instance) {
     		if (instance.hasProblem) {
 	    		$("#instance_problems_content .panel-body").append('<div xmlns="http://www.w3.org/1999/xhtml" class="popover instance" data-nodeid="'+instance.id+'"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content"></div></div>');
 	
 	    		var popoverElement = $("#instance_problems [data-nodeid='" + instance.id + "'].popover");
 	    		renderInstanceElement(popoverElement, instance, "problems");
+	    		problemInstancesFound = true;
     		}
     	});        	
+    	if (problemInstancesFound) {
+    		$("#instance_problems .collapse").addClass("in");
+    	}
         
         $("div.popover").popover();
         $("div.popover").show();
