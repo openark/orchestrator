1ff38f0fc2f60fef56166d427ff67eebcff991f7
snoach <snoach@outbrain.com>
2014-05-20 08:30:07 +0300

    turned problems "collapse" sidebar into dropdown (got rid of "position: absolute" issues)

--- resources/public/js/instance-problems.js
+++ resources/public/js/instance-problems.js
@@ -16,15 +16,15 @@ $(document).ready(function () {
         var problemInstancesFound = false;
     	instances.forEach(function (instance) {
     		if (instance.hasProblem) {
-	    		$("#instance_problems_content .panel-body").append('<div xmlns="http://www.w3.org/1999/xhtml" class="popover instance" data-nodeid="'+instance.id+'"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content"></div></div>');
-	
+	    		$("#instance_problems ul").append('<li><div xmlns="http://www.w3.org/1999/xhtml" class="popover instance" data-nodeid="'+instance.id+'"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content"></div></div></li>');
+
 	    		var popoverElement = $("#instance_problems [data-nodeid='" + instance.id + "'].popover");
 	    		renderInstanceElement(popoverElement, instance, "problems");
 	    		problemInstancesFound = true;
     		}
     	});        	
     	if (problemInstancesFound) {
-    		$("#instance_problems .collapse").addClass("in");
+    		$("#instance_problems .dropdown-toggle").dropdown('toggle');
     	}
         
         $("div.popover").popover();
