f48eec8e7e826a0c1a466ead9514b04078578dbd
snoach <snoach@outbrain.com>
2014-06-08 11:31:41 +0300

    added simple build/packaging script

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Simple packaging of orchestrator
+#
+# Requires fpm: https://github.com/jordansissel/fpm
+#
+
+release_dir=/tmp/orchestrator-release
+release_files_dir=$release_dir/orchestrator
+rm -rf $release_dir/*
+mkdir -p $release_files_dir/
+
+cd  $(dirname $0)
+rsync -av ./resources $release_files_dir/
+rsync -av ./conf $release_files_dir/
+GOPATH=/usr/share/golang:$(pwd)
+go build -o $release_files_dir/orchestrator ./src/github.com/outbrain/orchestrator/main.go
+
+cd $release_dir
+# tar packaging
+tar cfz orchestrator.tar.gz ./orchestrator
+# rpm packaging
+fpm -f -s dir -t rpm -n orchestrator -C $release_dir --prefix=/usr/local/orchestrator .
