a52f11b7595942cd4a2343c7a228cfab69212480
snoach <snoach@outbrain.com>
2014-09-17 17:36:23 +0300

    RPM and deb packages now include /etc/init.d/orchestrator service daemon

--- build.sh
+++ build.sh
@@ -5,24 +5,35 @@
 # Requires fpm: https://github.com/jordansissel/fpm
 #
 
-release_version="1.1.0"
+release_version="1.1.1"
 release_dir=/tmp/orchestrator-release
 release_files_dir=$release_dir/orchestrator
 rm -rf $release_dir/*
 mkdir -p $release_files_dir/
+mkdir -p $release_files_dir/usr/local
+mkdir -p $release_files_dir/etc/init.d
 
 cd  $(dirname $0)
-rsync -av ./resources $release_files_dir/
-rsync -av ./conf $release_files_dir/
+
+rsync -av ./resources $release_files_dir/usr/local/orchestrator/
+rsync -av ./conf $release_files_dir/usr/local/orchestrator/
+cp etc/init.d/orchestrator.bash $release_files_dir/etc/init.d/orchestrator
+chmod +x $release_files_dir/etc/init.d/orchestrator
+
+
 GOPATH=/usr/share/golang:$(pwd)
-go build -o $release_files_dir/orchestrator ./src/github.com/outbrain/orchestrator/main.go
+go build -o $release_files_dir/usr/local/orchestrator/orchestrator ./src/github.com/outbrain/orchestrator/main.go
+
+if [[ $? -ne 0 ]] ; then
+	exit 1
+fi
 
 cd $release_dir
 # tar packaging
 tar cfz orchestrator-"${release_version}".tar.gz ./orchestrator
 # rpm packaging
-fpm -v "${release_version}" -f -s dir -t rpm -n orchestrator -C $release_dir --prefix=/usr/local ./orchestrator
-fpm -v "${release_version}" -f -s dir -t deb -n orchestrator -C $release_dir --prefix=/usr/local ./orchestrator
+fpm -v "${release_version}" -f -s dir -t rpm -n orchestrator -C $release_dir/orchestrator --prefix=/ .
+fpm -v "${release_version}" -f -s dir -t deb -n orchestrator -C $release_dir/orchestrator --prefix=/ .
 
 echo "---"
 echo "Done. Find releases in $release_dir"
