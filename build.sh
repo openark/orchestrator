cbe9107313334ec3905814b625b0ba0176f2b5d1
shlomi-noach <shlomi@openark.org>
2015-02-09 13:08:12 +0100

    updated version

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 # Requires fpm: https://github.com/jordansissel/fpm
 #
 
-release_version="1.3.17"
+release_version="1.3.18"
 release_dir=/tmp/orchestrator-release
 release_files_dir=$release_dir/orchestrator
 release_files_cli_dir=$release_dir/orchestrator-cli
