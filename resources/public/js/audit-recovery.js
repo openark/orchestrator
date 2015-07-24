0641974e2a924528cd3c8b7686ef61c933e4d7a0
shlomi-noach <shlomi@openark.org>
2015-07-24 15:48:31 +0200

    web: - audit-failure-detection page was wrongly copied from audit-recovery; reverted - highlight for audit-recovery and audit-failure-detection - search page: instances titles clickable (open dialog) - refactored css a bit

--- resources/public/js/audit-recovery.js
+++ resources/public/js/audit-recovery.js
@@ -7,15 +7,17 @@ $(document).ready(function () {
     function displayAudit(auditEntries) {
         hideLoader();
         auditEntries.forEach(function (audit) {
+        	var analyzedInstanceDisplay = audit.AnalysisEntry.AnalyzedInstanceKey.Hostname+":"+audit.AnalysisEntry.AnalyzedInstanceKey.Port;
+        	var sucessorInstanceDisplay = audit.SuccessorKey.Hostname+":"+audit.SuccessorKey.Port;
     		var row = jQuery('<tr/>');
-    		jQuery('<td/>', { text: audit.AnalysisEntry.Analysis }).appendTo(row);
-    		jQuery('<td/>', { text: audit.AnalysisEntry.AnalyzedInstanceKey.Hostname+":"+audit.AnalysisEntry.AnalyzedInstanceKey.Port }).appendTo(row);
-    		jQuery('<td/>', { text: audit.AnalysisEntry.CountSlaves }).appendTo(row);
-    		jQuery('<td/>', { text: audit.AnalysisEntry.ClusterDetails.ClusterName }).appendTo(row);
-    		jQuery('<td/>', { text: audit.AnalysisEntry.ClusterDetails.ClusterAlias }).appendTo(row);
-    		jQuery('<td/>', { text: audit.RecoveryStartTimestamp }).appendTo(row);
-    		jQuery('<td/>', { text: audit.RecoveryEndTimestamp }).appendTo(row);
-    		jQuery('<td/>', { text: audit.SuccessorKey.Hostname+":"+audit.SuccessorKey.Port }).appendTo(row);
+    		$('<td/>', { text: audit.AnalysisEntry.Analysis }).appendTo(row);
+    		$('<a/>',  { text: analyzedInstanceDisplay, href: "/web/search/" + analyzedInstanceDisplay }).wrap($("<td/>")).parent().appendTo(row);
+    		$('<td/>', { text: audit.AnalysisEntry.CountSlaves }).appendTo(row);
+    		$('<a/>',  { text: audit.AnalysisEntry.ClusterDetails.ClusterName, href: "/web/cluster/"+audit.AnalysisEntry.ClusterDetails.ClusterName}).wrap($("<td/>")).parent().appendTo(row);
+    		$('<a/>',  { text: audit.AnalysisEntry.ClusterDetails.ClusterAlias, href: "/web/cluster/alias/"+audit.AnalysisEntry.ClusterDetails.ClusterAlias}).wrap($("<td/>")).parent().appendTo(row);
+    		$('<td/>', { text: audit.RecoveryStartTimestamp }).appendTo(row);
+    		$('<td/>', { text: audit.RecoveryEndTimestamp }).appendTo(row);
+    		$('<a/>',  { text: sucessorInstanceDisplay, href: "/web/search/" + sucessorInstanceDisplay }).wrap($("<td/>")).parent().appendTo(row);
     		row.appendTo('#audit tbody');
     	});
         if (currentPage() <= 0) {
