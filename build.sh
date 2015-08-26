0f170dba68f1254d967a41439b460448b6009539
John Cesario <cesario@squareup.com>
2015-08-26 11:49:25 -0700

    Support configurable install root for build
    Support -p to change build install root from /usr/local to other.

--- build.sh
+++ build.sh
@@ -12,12 +12,13 @@ export RELEASE_VERSION TOPDIR
 
 usage() {
   echo
-  echo "Usage: $0 [-t target ] [-a arch ] [-h] [-d]"
+  echo "Usage: $0 [-t target ] [-a arch ] [ -p prefix ] [-h] [-d]"
   echo "Options:"
   echo "-h Show this screen"
-  echo "-t (linux|darwin) Target OS. Default:(linux)"
+  echo "-t (linux|darwin) Target OS Default:(linux)"
   echo "-a (amd64|386) Arch Default:(amd64)"
   echo "-d debug output"
+  echo "-p build prefix Default:(/usr/local)"
   echo
 }
 
@@ -47,34 +48,37 @@ function precheck() {
 }
 
 function setuptree() {
-  local b
+  local b prefix
+  prefix="$1"
 
   mkdir -p $TOPDIR
-rm -rf $TOPDIR/*
+  rm -rf $TOPDIR/*
   b=$( mktemp -d $TOPDIR/orchestratorXXXXXX ) || return 1
   mkdir -p $b/orchestrator
-  mkdir -p $b/orchestrator/usr/local/orchestrator/
+  mkdir -p $b/orchestrator${prefix}/orchestrator/
   mkdir -p $b/orchestrator/etc/init.d
   mkdir -p $b/orchestrator-cli/usr/bin
   echo $b
 }
 
 function oinstall() {
-  local builddir
+  local builddir prefix
   builddir="$1"
+  prefix="$2"
 
   cd  $(dirname $0)
- gofmt -s -w  go/
-  rsync -qa ./resources $builddir/orchestrator/usr/local/orchestrator/
-  rsync -qa ./conf/*.sample $builddir/orchestrator/usr/local/orchestrator/
+  gofmt -s -w  go/
+  rsync -qa ./resources $builddir/orchestrator${prefix}/orchestrator/
+  rsync -qa ./conf/*.sample $builddir/orchestrator${prefix}/orchestrator/
   cp etc/init.d/orchestrator.bash $builddir/orchestrator/etc/init.d/orchestrator
   chmod +x $builddir/orchestrator/etc/init.d/orchestrator
 }
 
 function package() {
-  local target builddir packages
+  local target builddir prefix packages
   target="$1"
   builddir="$2"
+  prefix="$3"
 
   cd $TOPDIR
 
@@ -90,7 +94,7 @@ function package() {
       cd $TOPDIR
       # rpm packaging -- executable only
       echo "Creating Distro cli packages"
-      cp $builddir/orchestrator/usr/local/orchestrator/orchestrator $builddir/orchestrator-cli/usr/bin
+      cp $builddir/orchestrator${prefix}/orchestrator/orchestrator $builddir/orchestrator-cli/usr/bin
       fpm -v "${RELEASE_VERSION}" --epoch 1  -f -s dir -t rpm -n orchestrator-cli -C $builddir/orchestrator-cli --prefix=/ .
       fpm -v "${RELEASE_VERSION}" --epoch 1  -f -s dir -t deb -n orchestrator-cli -C $builddir/orchestrator-cli --prefix=/ .
       ;;
@@ -98,7 +102,7 @@ function package() {
       echo "Creating Darwin full Package"
       tar -C $builddir/orchestrator -czf $TOPDIR/orchestrator-"${RELEASE_VERSION}"-$target-$arch.tar.gz ./
       echo "Creating Darwin cli Package"
-      cp $builddir/orchestrator/usr/local/orchestrator/orchestrator $builddir/orchestrator-cli/usr/bin
+      cp $builddir/orchestrator${prefix}/orchestrator/orchestrator $builddir/orchestrator-cli/usr/bin
       tar -C $builddir/orchestrator-cli -czf $TOPDIR/orchestrator-cli-"${RELEASE_VERSION}"-$target-$arch.tar.gz ./
       ;;
   esac
@@ -108,12 +112,12 @@ function package() {
 }
 
 function build() {
-  local target arch builddir gobuild
-
+  local target arch builddir gobuild prefix
   os="$1"
   arch="$2"
   builddir="$3"
-  gobuild="go build -o $builddir/orchestrator/usr/local/orchestrator/orchestrator go/cmd/orchestrator/main.go"
+  prefix="$4"
+  gobuild="go build -o $builddir/orchestrator${prefix}/orchestrator/orchestrator go/cmd/orchestrator/main.go"
 
   case $os in
     'linux')
@@ -126,25 +130,26 @@ function build() {
 }
 
 function main() {
-  local target arch builddir
+  local target arch builddir prefix
   target="$1"
   arch="$2"
+  prefix="$3"
 
   precheck
-  builddir=$( setuptree )
-  oinstall $builddir
-  build $target $arch $builddir
-  package $target $builddir
+  builddir=$( setuptree "$prefix" )
+  oinstall "$builddir" "$prefix"
+  build "$target" "$arch" "$builddir" "$prefix"
+  package "$target" "$builddir" "$prefix"
   # cleanup
 }
 
-while getopts a:t:dh flag; do
+while getopts a:t:p:dh flag; do
   case $flag in
   a)
-    arch=$OPTARG
+    arch="$OPTARG"
     ;;
   t)
-    target=$OPTARG
+    target="$OPTARG"
     ;;
   h)
     usage
@@ -152,6 +157,9 @@ while getopts a:t:dh flag; do
     ;;
   d)
     debug=1
+    ;;
+  p)
+    prefix="$OPTARG"
     ;;
   ?)
     usage
@@ -163,7 +171,8 @@ done
 shift $(( OPTIND - 1 ));
 target=${target:-"linux"} # default for target is linux
 arch=${arch:-"amd64"} # default for arch is amd64
+prefix=${prefix:-"/usr/local"}
 
 [[ $debug -eq 1 ]] && set -x
-main $target $arch
+main "$target" "$arch" "$prefix"
 
