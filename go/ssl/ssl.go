8f1bafd471fced292679ab554010599a17549a0d
Grier Johnson <grier@squareup.com>
2015-12-01 13:09:20 -0800

    Bumping default minimum TLS to TLS 1.2 Dropping minimum to 1.0 for MySQL connections Removing insecure ciphers

--- go/ssl/ssl.go
+++ go/ssl/ssl.go
@@ -13,6 +13,19 @@ import (
 	"github.com/outbrain/orchestrator/go/config"
 )
 
+var cipherSuites = []uint16{
+	tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+	tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+	tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+	tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+	tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+	tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+	tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+	tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+	tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+	tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+}
+
 // Determine if a string element is in a string array
 func HasString(elem string, arr []string) bool {
 	for _, s := range arr {
@@ -29,7 +42,9 @@ func NewTLSConfig(caFile string, mutualTLS bool) (*tls.Config, error) {
 	var c tls.Config
 
 	// TLS 1.0 at a minimum (for mysql)
-	c.MinVersion = tls.VersionTLS10
+	c.MinVersion = tls.VersionTLS12
+	// Remove insecure ciphers from the list
+	c.CipherSuites = cipherSuites
 	c.PreferServerCipherSuites = true
 
 	if mutualTLS {
