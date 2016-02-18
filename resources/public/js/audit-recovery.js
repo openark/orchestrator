7307dcceb9f36a25dc747660ef4801960d71e7f3
Shlomi Noach <shlomi-noach@github.com>
2016-02-18 07:19:29 -0500

    beautified JS files

--- resources/public/js/audit-recovery.js
+++ resources/public/js/audit-recovery.js
@@ -1,144 +1,168 @@
+$(document).ready(function() {
+  showLoader();
+  var apiUri = "/api/audit-recovery/" + currentPage();
+  if (auditCluster()) {
+    apiUri = "/api/audit-recovery/cluster/" + auditCluster() + "/" + currentPage();
+  }
+  if (recoveryId() > 0) {
+    apiUri = "/api/audit-recovery/id/" + recoveryId();
+  }
+  $.get(apiUri, function(auditEntries) {
+    displayAudit(auditEntries);
+  }, "json");
 
-$(document).ready(function () {
-    showLoader();
-    var apiUri = "/api/audit-recovery/"+currentPage();
+  function displayAudit(auditEntries) {
+    var baseWebUri = "/web/audit-recovery/";
     if (auditCluster()) {
-    	apiUri = "/api/audit-recovery/cluster/"+auditCluster()+"/"+currentPage();
+      baseWebUri += "cluster/" + auditCluster() + "/";
     }
-    if (recoveryId() > 0) {
-    	apiUri = "/api/audit-recovery/id/"+recoveryId();
-    }
-    $.get(apiUri, function (auditEntries) {
-    	displayAudit(auditEntries);
-    }, "json");
-    function displayAudit(auditEntries) {
-    	var baseWebUri = "/web/audit-recovery/";
-    	if (auditCluster()) {
-    		baseWebUri += "cluster/"+auditCluster()+"/";
-        }
 
-    	hideLoader();
-        auditEntries.forEach(function (audit) {
-        	var analyzedInstanceDisplay = getInstanceTitle(audit.AnalysisEntry.AnalyzedInstanceKey.Hostname, audit.AnalysisEntry.AnalyzedInstanceKey.Port);
-        	var sucessorInstanceDisplay = getInstanceTitle(audit.SuccessorKey.Hostname, audit.SuccessorKey.Port);
-    		var row = $('<tr/>');
-    		var ack = $('<span class="pull-left glyphicon acknowledge-indicator" title=""></span>');
-    		if (audit.Acknowledged) {
-    			ack.addClass("glyphicon-ok-sign").addClass("text-primary");
-    			var ackTitle = "Acknowledged by "+audit.AcknowledgedBy+" at "+audit.AcknowledgedAt+": "+audit.AcknowledgedComment;
-    			ack.attr("title", ackTitle);
-    		} else {
-    			ack.addClass("glyphicon-question-sign").addClass("text-danger").addClass("unacknowledged");
-    			ack.attr("data-recovery-id", audit.Id);
-    			ack.attr("title", "Unacknowledged. Click to acknowledge");
-    		}
-    		var moreInfoElement = $('<span class="more-recovery-info pull-right glyphicon glyphicon-info-sign text-primary" title="More info"></span>');
-    		moreInfoElement.attr("data-recovery-id", audit.Id);
-    		
-    		$('<td/>', { text: audit.AnalysisEntry.Analysis }).prepend(ack).prepend(moreInfoElement).appendTo(row);
-    		$('<a/>',  { text: analyzedInstanceDisplay, href: "/web/search/" + analyzedInstanceDisplay }).wrap($("<td/>")).parent().appendTo(row);
-    		$('<td/>', { text: audit.AnalysisEntry.CountSlaves }).appendTo(row);
-    		$('<a/>',  { text: audit.AnalysisEntry.ClusterDetails.ClusterName, href: "/web/cluster/"+audit.AnalysisEntry.ClusterDetails.ClusterName}).wrap($("<td/>")).parent().appendTo(row);
-    		$('<a/>',  { text: audit.AnalysisEntry.ClusterDetails.ClusterAlias, href: "/web/cluster/alias/"+audit.AnalysisEntry.ClusterDetails.ClusterAlias}).wrap($("<td/>")).parent().appendTo(row);
-    		$('<td/>', { text: audit.RecoveryStartTimestamp }).appendTo(row);
-    		$('<td/>', { text: audit.RecoveryEndTimestamp }).appendTo(row);
-    		if (audit.RecoveryEndTimestamp && !audit.IsSuccessful && !audit.SuccessorKey.Hostname) {
-        		$('<td/>', { text: "FAIL" }).appendTo(row);
-    		} else if (audit.SuccessorKey.Hostname) {
-    			$('<a/>',  { text: sucessorInstanceDisplay, href: "/web/search/" + sucessorInstanceDisplay }).wrap($("<td/>")).parent().appendTo(row);
-    		} else {
-    			$('<td/>', { text: "pending" }).appendTo(row);
-    		}
-    		var moreInfo = "";
-    		if (audit.Acknowledged) {
-    			moreInfo += '<div>Acknowledged by '+audit.AcknowledgedBy+', '+audit.AcknowledgedAt+'<ul>';
-    			moreInfo += "<li>"+audit.AcknowledgedComment+"</li>";    		
-    			moreInfo += '</ul></div>';
-    		} else {
-    			moreInfo += '<div><strong>Unacknowledged</strong></div>';
-    		}
-    		if (audit.LostSlaves.length > 0) {
-    			moreInfo += "<div>Lost slaves:<ul>";
-        		audit.LostSlaves.forEach(function(instanceKey) {
-        			moreInfo += "<li><code>"+getInstanceTitle(instanceKey.Hostname, instanceKey.Port)+"</code></li>";    			
-        		});
-        		moreInfo += "</ul></div>";
-    		}
-    		if (audit.ParticipatingInstanceKeys.length > 0) {
-    			moreInfo += "<div>Participating instances:<ul>";
-        		audit.ParticipatingInstanceKeys.forEach(function(instanceKey) {
-        			moreInfo += "<li><code>"+getInstanceTitle(instanceKey.Hostname, instanceKey.Port)+"</code></li>";    			
-        		});
-        		moreInfo += "</ul></div>";
-    		}
-    		if (audit.AnalysisEntry.SlaveHosts.length > 0) {
-    			moreInfo += '<div>'+audit.AnalysisEntry.CountSlaves+' slave hosts :<ul>';
-        		audit.AnalysisEntry.SlaveHosts.forEach(function(instanceKey) {
-        			moreInfo += "<li><code>"+getInstanceTitle(instanceKey.Hostname, instanceKey.Port)+"</code></li>";    			
-        		});
-        		moreInfo += "</ul></div>";
-    		}
-    		if (audit.AllErrors.length > 0 && audit.AllErrors[0]) {
-    			moreInfo += "All errors:<ul>";
-        		audit.AllErrors.forEach(function(err) {
-        			moreInfo += "<li>"+err;    			
-        		});
-        		moreInfo += "</ul>";
-    		}
-    		moreInfo += '<div><a href="/web/audit-failure-detection/id/' + audit.LastDetectionId + '">Related detection</a></div>';
-    		moreInfo += '<div>Proccessed by <code>'+audit.ProcessingNodeHostname+'</code></div>';
-    		row.appendTo('#audit tbody');
-    		
-    		var row = $('<tr/>');
-    		row.addClass("more-info");
-			row.attr("data-recovery-id-more-info", audit.Id);
-    		$('<td colspan="8"/>').append(moreInfo).appendTo(row);
-    		if (audit.Acknowledged) {
-    			row.hide()
-    		}
-    		row.appendTo('#audit tbody');
-    	});
-        if (auditEntries.length == 1) {
-        	$("[data-recovery-id-more-info]").show();
-        }
-        if (currentPage() <= 0) {
-        	$("#audit .pager .previous").addClass("disabled");
-        }
-        if (auditEntries.length == 0) {
-        	$("#audit .pager .next").addClass("disabled");        	
-        }
-        $("#audit .pager .previous").not(".disabled").find("a").click(function() {
-            window.location.href = baseWebUri+(currentPage() - 1);
+    hideLoader();
+    auditEntries.forEach(function(audit) {
+      var analyzedInstanceDisplay = getInstanceTitle(audit.AnalysisEntry.AnalyzedInstanceKey.Hostname, audit.AnalysisEntry.AnalyzedInstanceKey.Port);
+      var sucessorInstanceDisplay = getInstanceTitle(audit.SuccessorKey.Hostname, audit.SuccessorKey.Port);
+      var row = $('<tr/>');
+      var ack = $('<span class="pull-left glyphicon acknowledge-indicator" title=""></span>');
+      if (audit.Acknowledged) {
+        ack.addClass("glyphicon-ok-sign").addClass("text-primary");
+        var ackTitle = "Acknowledged by " + audit.AcknowledgedBy + " at " + audit.AcknowledgedAt + ": " + audit.AcknowledgedComment;
+        ack.attr("title", ackTitle);
+      } else {
+        ack.addClass("glyphicon-question-sign").addClass("text-danger").addClass("unacknowledged");
+        ack.attr("data-recovery-id", audit.Id);
+        ack.attr("title", "Unacknowledged. Click to acknowledge");
+      }
+      var moreInfoElement = $('<span class="more-recovery-info pull-right glyphicon glyphicon-info-sign text-primary" title="More info"></span>');
+      moreInfoElement.attr("data-recovery-id", audit.Id);
+
+      $('<td/>', {
+        text: audit.AnalysisEntry.Analysis
+      }).prepend(ack).prepend(moreInfoElement).appendTo(row);
+      $('<a/>', {
+        text: analyzedInstanceDisplay,
+        href: "/web/search/" + analyzedInstanceDisplay
+      }).wrap($("<td/>")).parent().appendTo(row);
+      $('<td/>', {
+        text: audit.AnalysisEntry.CountSlaves
+      }).appendTo(row);
+      $('<a/>', {
+        text: audit.AnalysisEntry.ClusterDetails.ClusterName,
+        href: "/web/cluster/" + audit.AnalysisEntry.ClusterDetails.ClusterName
+      }).wrap($("<td/>")).parent().appendTo(row);
+      $('<a/>', {
+        text: audit.AnalysisEntry.ClusterDetails.ClusterAlias,
+        href: "/web/cluster/alias/" + audit.AnalysisEntry.ClusterDetails.ClusterAlias
+      }).wrap($("<td/>")).parent().appendTo(row);
+      $('<td/>', {
+        text: audit.RecoveryStartTimestamp
+      }).appendTo(row);
+      $('<td/>', {
+        text: audit.RecoveryEndTimestamp
+      }).appendTo(row);
+      if (audit.RecoveryEndTimestamp && !audit.IsSuccessful && !audit.SuccessorKey.Hostname) {
+        $('<td/>', {
+          text: "FAIL"
+        }).appendTo(row);
+      } else if (audit.SuccessorKey.Hostname) {
+        $('<a/>', {
+          text: sucessorInstanceDisplay,
+          href: "/web/search/" + sucessorInstanceDisplay
+        }).wrap($("<td/>")).parent().appendTo(row);
+      } else {
+        $('<td/>', {
+          text: "pending"
+        }).appendTo(row);
+      }
+      var moreInfo = "";
+      if (audit.Acknowledged) {
+        moreInfo += '<div>Acknowledged by ' + audit.AcknowledgedBy + ', ' + audit.AcknowledgedAt + '<ul>';
+        moreInfo += "<li>" + audit.AcknowledgedComment + "</li>";
+        moreInfo += '</ul></div>';
+      } else {
+        moreInfo += '<div><strong>Unacknowledged</strong></div>';
+      }
+      if (audit.LostSlaves.length > 0) {
+        moreInfo += "<div>Lost slaves:<ul>";
+        audit.LostSlaves.forEach(function(instanceKey) {
+          moreInfo += "<li><code>" + getInstanceTitle(instanceKey.Hostname, instanceKey.Port) + "</code></li>";
         });
-        $("#audit .pager .next").not(".disabled").find("a").click(function() {
-            window.location.href = baseWebUri+(currentPage() + 1);
+        moreInfo += "</ul></div>";
+      }
+      if (audit.ParticipatingInstanceKeys.length > 0) {
+        moreInfo += "<div>Participating instances:<ul>";
+        audit.ParticipatingInstanceKeys.forEach(function(instanceKey) {
+          moreInfo += "<li><code>" + getInstanceTitle(instanceKey.Hostname, instanceKey.Port) + "</code></li>";
         });
-        $("#audit .pager .disabled a").click(function() {
-            return false;
+        moreInfo += "</ul></div>";
+      }
+      if (audit.AnalysisEntry.SlaveHosts.length > 0) {
+        moreInfo += '<div>' + audit.AnalysisEntry.CountSlaves + ' slave hosts :<ul>';
+        audit.AnalysisEntry.SlaveHosts.forEach(function(instanceKey) {
+          moreInfo += "<li><code>" + getInstanceTitle(instanceKey.Hostname, instanceKey.Port) + "</code></li>";
         });
-        $("body").on("click", ".more-recovery-info", function (event) {
-        	var recoveryId = $(event.target).attr("data-recovery-id");
-            $('[data-recovery-id-more-info='+recoveryId+']').slideToggle();        
+        moreInfo += "</ul></div>";
+      }
+      if (audit.AllErrors.length > 0 && audit.AllErrors[0]) {
+        moreInfo += "All errors:<ul>";
+        audit.AllErrors.forEach(function(err) {
+          moreInfo += "<li>" + err;
         });
-        $("body").on("click", ".acknowledge-indicator.unacknowledged", function (event) {
-        	var recoveryId = $(event.target).attr("data-recovery-id");
-            bootbox.prompt({
-                title: "Acknowledge recovery",
-                placeholder: "comment",
-                callback: function (result) {
-                    if (result !== null) {
-                        showLoader();
-                        $.get("/api/ack-recovery/"+recoveryId+"?comment="+encodeURIComponent(result), function (operationResult) {
-                            hideLoader();
-                            if (operationResult.Code == "ERROR") {
-                                addAlert(operationResult.Message)
-                            } else {
-                                location.reload();
-                            }
-                        }, "json");
-                    }
-                }
-            });
-        });
+        moreInfo += "</ul>";
+      }
+      moreInfo += '<div><a href="/web/audit-failure-detection/id/' + audit.LastDetectionId + '">Related detection</a></div>';
+      moreInfo += '<div>Proccessed by <code>' + audit.ProcessingNodeHostname + '</code></div>';
+      row.appendTo('#audit tbody');
+
+      var row = $('<tr/>');
+      row.addClass("more-info");
+      row.attr("data-recovery-id-more-info", audit.Id);
+      $('<td colspan="8"/>').append(moreInfo).appendTo(row);
+      if (audit.Acknowledged) {
+        row.hide()
+      }
+      row.appendTo('#audit tbody');
+    });
+    if (auditEntries.length == 1) {
+      $("[data-recovery-id-more-info]").show();
     }
-});	
+    if (currentPage() <= 0) {
+      $("#audit .pager .previous").addClass("disabled");
+    }
+    if (auditEntries.length == 0) {
+      $("#audit .pager .next").addClass("disabled");
+    }
+    $("#audit .pager .previous").not(".disabled").find("a").click(function() {
+      window.location.href = baseWebUri + (currentPage() - 1);
+    });
+    $("#audit .pager .next").not(".disabled").find("a").click(function() {
+      window.location.href = baseWebUri + (currentPage() + 1);
+    });
+    $("#audit .pager .disabled a").click(function() {
+      return false;
+    });
+    $("body").on("click", ".more-recovery-info", function(event) {
+      var recoveryId = $(event.target).attr("data-recovery-id");
+      $('[data-recovery-id-more-info=' + recoveryId + ']').slideToggle();
+    });
+    $("body").on("click", ".acknowledge-indicator.unacknowledged", function(event) {
+      var recoveryId = $(event.target).attr("data-recovery-id");
+      bootbox.prompt({
+        title: "Acknowledge recovery",
+        placeholder: "comment",
+        callback: function(result) {
+          if (result !== null) {
+            showLoader();
+            $.get("/api/ack-recovery/" + recoveryId + "?comment=" + encodeURIComponent(result), function(operationResult) {
+              hideLoader();
+              if (operationResult.Code == "ERROR") {
+                addAlert(operationResult.Message)
+              } else {
+                location.reload();
+              }
+            }, "json");
+          }
+        }
+      });
+    });
+  }
+});
--- resources/public/js/cluster-pools.js
+++ resources/public/js/cluster-pools.js
@@ -1,140 +1,157 @@
+$(document).ready(function() {
+  var isExpanded = true;
 
-$(document).ready(function () {
-	var isExpanded = true;
-	
-    showLoader();
-    
-    var errorMapping = {
-   		"inMaintenanceProblem": {"badge": "label-info", "description": "In maintenance"}, 
-   		"lastCheckInvalidProblem": {"badge": "label-fatal", "description": "Last check invalid"}, 
-   		"notRecentlyCheckedProblem": {"badge": "label-stale", "description": "Not recently checked (stale)"}, 
-   		"notReplicatingProblem": {"badge": "label-danger", "description": "Not replicating"}, 
-   		"replicationLagProblem": {"badge": "label-warning", "description": "Replication lag"}
-	};
-    
-    $.get("/api/cluster-pool-instances/" + currentClusterName(), function (clusterPoolInstances) {
-        $.get("/api/problems", function (problemInstances) {
-        	var problemInstancesMap = normalizeInstances(problemInstances, []);
-	    	displayClusterPoolInstances(clusterPoolInstances, problemInstances, problemInstancesMap);
-        }, "json");
+  showLoader();
+
+  var errorMapping = {
+    "inMaintenanceProblem": {
+      "badge": "label-info",
+      "description": "In maintenance"
+    },
+    "lastCheckInvalidProblem": {
+      "badge": "label-fatal",
+      "description": "Last check invalid"
+    },
+    "notRecentlyCheckedProblem": {
+      "badge": "label-stale",
+      "description": "Not recently checked (stale)"
+    },
+    "notReplicatingProblem": {
+      "badge": "label-danger",
+      "description": "Not replicating"
+    },
+    "replicationLagProblem": {
+      "badge": "label-warning",
+      "description": "Replication lag"
+    }
+  };
+
+  $.get("/api/cluster-pool-instances/" + currentClusterName(), function(clusterPoolInstances) {
+    $.get("/api/problems", function(problemInstances) {
+      var problemInstancesMap = normalizeInstances(problemInstances, []);
+      displayClusterPoolInstances(clusterPoolInstances, problemInstances, problemInstancesMap);
     }, "json");
-    function sortByCountInstances(pool1, pool2) {
-    	var diff = pool2.instances.length - pool1.instances.length;
-    	if (diff != 0) {
-    		return diff;
-    	}
-    	return pool1.name.localeCompare(pool2.name);
+  }, "json");
+
+  function sortByCountInstances(pool1, pool2) {
+    var diff = pool2.instances.length - pool1.instances.length;
+    if (diff != 0) {
+      return diff;
     }
-    
-    function displayClusterPoolInstances(clusterPoolInstances, problemInstances, problemInstancesMap) {
-        hideLoader();
-        
-        var poolsProblems = {};
-        var pools = new Array();
-    	for (var pool in clusterPoolInstances.Details) {
-    		if (clusterPoolInstances.Details.hasOwnProperty(pool)) {
-    			poolsProblems[pool] = {};
-    			pools.push({name: pool, instances: clusterPoolInstances.Details[pool]});
-    		}
-   		}
-        pools.sort(sortByCountInstances);
-        
-	    function addInstancesBadge(poolName, count, badgeClass, title) {
-	    	$("#pools [data-pool-name='" + poolName + "'].popover").find(".popover-content .pull-right").append('<span class="badge '+badgeClass+'" title="' + title + '">' + count + '</span> ');
-	    }
-        
-        function incrementPoolProblems(poolName, problemType) {
-        	if (poolsProblems[poolName][problemType] > 0) {
-        		poolsProblems[poolName][problemType] = poolsProblems[poolName][problemType] + 1;
-        	} else {
-        		poolsProblems[poolName][problemType] = 1;
-        	}
-        }
-        function incrementPoolsProblems(instance, problemType) {
-        	if (typeof instance.problemHint === 'undefined') {
-        		instance.problemHint = problemType
-        	}
-            pools.forEach(function (pool) {
-            	pool.instances.forEach(function (poolInstance) {
-            		if ((poolInstance.Hostname == instance.Key.Hostname) && (poolInstance.Port = instance.Key.Port)) {
-            			incrementPoolProblems(pool.name, problemType)
-            		}
-            	});
-            });
-        }
-        problemInstances.forEach(function(instance) {
-	        if (instance.inMaintenanceProblem()) {
-	        	incrementPoolsProblems(instance, "inMaintenanceProblem")
-	        }
-	        //
-	        if (instance.lastCheckInvalidProblem()) {
-	        	incrementPoolsProblems(instance, "lastCheckInvalidProblem")
-	        } else if (instance.notRecentlyCheckedProblem()) {
-	        	incrementPoolsProblems(instance, "notRecentlyCheckedProblem")
-	        } else if (instance.notReplicatingProblem()) {
-	        	incrementPoolsProblems(instance, "notReplicatingProblem")
-	        } else if (instance.replicationLagProblem()) {
-	        	incrementPoolsProblems(instance, "replicationLagProblem")
-	        }
-	    });
+    return pool1.name.localeCompare(pool2.name);
+  }
 
-        pools.forEach(function (pool) {
-    		$("#pools").append('<div xmlns="http://www.w3.org/1999/xhtml" class="popover instance right" data-pool-name="'+pool.name+'"><div class="arrow"></div><h3 class="popover-title"><div class="pull-left"><span>'+pool.name+'</span></div><div class="pull-right"></div>&nbsp;<br/>&nbsp;</h3><div class="popover-content"></div></div>');
-    		var popoverElement = $("#pools [data-pool-name='" + pool.name + "'].popover");
+  function displayClusterPoolInstances(clusterPoolInstances, problemInstances, problemInstancesMap) {
+    hideLoader();
 
-    	    var contentHtml = ''
-    				+ '<div>Instances: <div class="pull-right"></div><div class="pool-instances-listing"></div></div>'
-    			;
-    	    popoverElement.find(".popover-content").html(contentHtml);
-    	    addInstancesBadge(pool.name, pool.instances.length, "label-primary", "Total instances in pool");
-    	    for (var problemType in poolsProblems[pool.name]) {
-    	    	addInstancesBadge(pool.name, poolsProblems[pool.name][problemType], errorMapping[problemType]["badge"], errorMapping[problemType]["description"]);
-    	    }
-    	    pool.instances.forEach(function(instance) {
-    	    	var instanceId = getInstanceId(instance.Hostname, instance.Port);
-    	    	var problemInstance = problemInstancesMap[instanceId];
-    	    	var instanceDisplay = instance.Hostname+":"+instance.Port;
-    	    	if (typeof removeTextFromHostnameDisplay != "undefined" && removeTextFromHostnameDisplay()) {
-    	    		instanceDisplay = instanceDisplay.replace(removeTextFromHostnameDisplay(), '');
-    	        }
-    	    	var instanceContent = "<div>";
-    	    	if (problemInstance && problemInstance.problemHint) {
-    	    		instanceContent += '<span class="badge '+errorMapping[problemInstance.problemHint]["badge"]+'" title="'+errorMapping[problemInstance.problemHint]["description"]+'">&nbsp;</span> '
-    	    	}
-    	    	instanceContent += instanceDisplay;
-    	    	instanceContent += "</div>";
-    	    	popoverElement.find("div.pool-instances-listing").append(instanceContent);
-    	    });
-        });     
-        
-        $("div.popover").popover();
-        $("div.popover").show();
-	
-        if (pools.length == 0) {
-        	addAlert("No pools found");
-        }
+    var poolsProblems = {};
+    var pools = new Array();
+    for (var pool in clusterPoolInstances.Details) {
+      if (clusterPoolInstances.Details.hasOwnProperty(pool)) {
+        poolsProblems[pool] = {};
+        pools.push({
+          name: pool,
+          instances: clusterPoolInstances.Details[pool]
+        });
+      }
+    }
+    pools.sort(sortByCountInstances);
+
+    function addInstancesBadge(poolName, count, badgeClass, title) {
+      $("#pools [data-pool-name='" + poolName + "'].popover").find(".popover-content .pull-right").append('<span class="badge ' + badgeClass + '" title="' + title + '">' + count + '</span> ');
     }
 
-    if (isAuthorizedForAction()) {
-    	// Read-only users don't get auto-refresh. Sorry!
-    	activateRefreshTimer();
+    function incrementPoolProblems(poolName, problemType) {
+      if (poolsProblems[poolName][problemType] > 0) {
+        poolsProblems[poolName][problemType] = poolsProblems[poolName][problemType] + 1;
+      } else {
+        poolsProblems[poolName][problemType] = 1;
+      }
     }
-    $("#dropdown-context").append('<li><a data-command="expand-instances">Expand</a></li>');    
-    $("#dropdown-context").append('<li><a href="/web/cluster/'+currentClusterName()+'">Topology</a></li>');    
-    $("body").on("click", "a[data-command=expand-instances]", function(event) {
-	    isExpanded = !isExpanded;
-	    updateExpandedStatus();
-    });  
-    
-    function updateExpandedStatus() {
-        if (isExpanded) {
-    	    $("div.pool-instances-listing").removeClass('hidden');
-        	$("#dropdown-context a[data-command=expand-instances]").prepend('<span class="glyphicon glyphicon-ok"></span> ');
-        } else {
-    	    $("div.pool-instances-listing").addClass('hidden');
-        	$("#dropdown-context a[data-command=expand-instances] span").remove();
+
+    function incrementPoolsProblems(instance, problemType) {
+      if (typeof instance.problemHint === 'undefined') {
+        instance.problemHint = problemType
+      }
+      pools.forEach(function(pool) {
+        pool.instances.forEach(function(poolInstance) {
+          if ((poolInstance.Hostname == instance.Key.Hostname) && (poolInstance.Port = instance.Key.Port)) {
+            incrementPoolProblems(pool.name, problemType)
+          }
+        });
+      });
+    }
+    problemInstances.forEach(function(instance) {
+      if (instance.inMaintenanceProblem()) {
+        incrementPoolsProblems(instance, "inMaintenanceProblem")
+      }
+      //
+      if (instance.lastCheckInvalidProblem()) {
+        incrementPoolsProblems(instance, "lastCheckInvalidProblem")
+      } else if (instance.notRecentlyCheckedProblem()) {
+        incrementPoolsProblems(instance, "notRecentlyCheckedProblem")
+      } else if (instance.notReplicatingProblem()) {
+        incrementPoolsProblems(instance, "notReplicatingProblem")
+      } else if (instance.replicationLagProblem()) {
+        incrementPoolsProblems(instance, "replicationLagProblem")
+      }
+    });
+
+    pools.forEach(function(pool) {
+      $("#pools").append('<div xmlns="http://www.w3.org/1999/xhtml" class="popover instance right" data-pool-name="' + pool.name + '"><div class="arrow"></div><h3 class="popover-title"><div class="pull-left"><span>' + pool.name + '</span></div><div class="pull-right"></div>&nbsp;<br/>&nbsp;</h3><div class="popover-content"></div></div>');
+      var popoverElement = $("#pools [data-pool-name='" + pool.name + "'].popover");
+
+      var contentHtml = '' + '<div>Instances: <div class="pull-right"></div><div class="pool-instances-listing"></div></div>';
+      popoverElement.find(".popover-content").html(contentHtml);
+      addInstancesBadge(pool.name, pool.instances.length, "label-primary", "Total instances in pool");
+      for (var problemType in poolsProblems[pool.name]) {
+        addInstancesBadge(pool.name, poolsProblems[pool.name][problemType], errorMapping[problemType]["badge"], errorMapping[problemType]["description"]);
+      }
+      pool.instances.forEach(function(instance) {
+        var instanceId = getInstanceId(instance.Hostname, instance.Port);
+        var problemInstance = problemInstancesMap[instanceId];
+        var instanceDisplay = instance.Hostname + ":" + instance.Port;
+        if (typeof removeTextFromHostnameDisplay != "undefined" && removeTextFromHostnameDisplay()) {
+          instanceDisplay = instanceDisplay.replace(removeTextFromHostnameDisplay(), '');
         }
+        var instanceContent = "<div>";
+        if (problemInstance && problemInstance.problemHint) {
+          instanceContent += '<span class="badge ' + errorMapping[problemInstance.problemHint]["badge"] + '" title="' + errorMapping[problemInstance.problemHint]["description"] + '">&nbsp;</span> '
+        }
+        instanceContent += instanceDisplay;
+        instanceContent += "</div>";
+        popoverElement.find("div.pool-instances-listing").append(instanceContent);
+      });
+    });
+
+    $("div.popover").popover();
+    $("div.popover").show();
+
+    if (pools.length == 0) {
+      addAlert("No pools found");
     }
-    
+  }
+
+  if (isAuthorizedForAction()) {
+    // Read-only users don't get auto-refresh. Sorry!
+    activateRefreshTimer();
+  }
+  $("#dropdown-context").append('<li><a data-command="expand-instances">Expand</a></li>');
+  $("#dropdown-context").append('<li><a href="/web/cluster/' + currentClusterName() + '">Topology</a></li>');
+  $("body").on("click", "a[data-command=expand-instances]", function(event) {
+    isExpanded = !isExpanded;
     updateExpandedStatus();
-});	
+  });
+
+  function updateExpandedStatus() {
+    if (isExpanded) {
+      $("div.pool-instances-listing").removeClass('hidden');
+      $("#dropdown-context a[data-command=expand-instances]").prepend('<span class="glyphicon glyphicon-ok"></span> ');
+    } else {
+      $("div.pool-instances-listing").addClass('hidden');
+      $("#dropdown-context a[data-command=expand-instances] span").remove();
+    }
+  }
+
+  updateExpandedStatus();
+});
