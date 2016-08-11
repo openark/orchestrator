55f3adfd7edb351a748045d429a9460d9738f848
Dmitry Voronov <dmitry.voronov@booking.com>
2016-08-11 10:17:25 +0200

    including go-sql-driver back to source
    It makes sense to depend less on external parties.
    If they move code around Orchestrator should still
    compile.
    
    So 'govendor' is not needed to build the code.

--- build.sh
+++ build.sh
@@ -51,11 +51,6 @@ function precheck() {
     ok=1
   fi
 
-  if [[ ! -x "$( which govendor )" ]]; then
-    echo "govendor not in PATH (install: go get -u github.com/kardianos/govendor)"
-    ok=1
-  fi
-
   if [[ $(go version | egrep "go1[.][01234]") ]]; then
     echo "go version is too low. Must use 1.5 or above"
     ok=1
@@ -158,10 +153,6 @@ function main() {
   build_only=$4
 
   precheck "$target"
-
-  # fetch dependencies
-  govendor sync
-
   builddir=$( setuptree "$prefix" )
   oinstall "$builddir" "$prefix"
   build "$target" "$arch" "$builddir" "$prefix"
