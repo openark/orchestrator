f32c78797709e6fc5243972971e933801bd12d92
Shlomi Noach <shlomi-noach@github.com>
2016-11-07 13:00:38 +0100

    slave->replica terminology, dual support in HTTP API

---
+++ go/http/api_test.go
@@ -0,0 +1,53 @@
+package http
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/go-martini/martini"
+
+	"github.com/outbrain/golib/log"
+	test "github.com/outbrain/golib/tests"
+	"github.com/outbrain/orchestrator/go/config"
+)
+
+func init() {
+	config.Config.HostnameResolveMethod = "none"
+	log.SetLevel(log.ERROR)
+}
+
+func TestGetSynonymPath(t *testing.T) {
+	api := HttpAPI{}
+
+	{
+		path := "relocate-slaves"
+		synonym := api.getSynonymPath(path)
+		test.S(t).ExpectEquals(synonym, "relocate-replicas")
+	}
+	{
+		path := "relocate-slaves/:host/:port"
+		synonym := api.getSynonymPath(path)
+		test.S(t).ExpectEquals(synonym, "relocate-replicas/:host/:port")
+	}
+}
+
+func TestKnownPaths(t *testing.T) {
+	m := martini.Classic()
+	api := HttpAPI{}
+
+	api.RegisterRequests(m)
+
+	pathsMap := make(map[string]bool)
+	for _, path := range registeredPaths {
+		pathBase := strings.Split(path, "/")[0]
+		pathsMap[pathBase] = true
+	}
+	test.S(t).ExpectTrue(pathsMap["health"])
+	test.S(t).ExpectTrue(pathsMap["lb-check"])
+	test.S(t).ExpectTrue(pathsMap["relocate"])
+	test.S(t).ExpectTrue(pathsMap["relocate-slaves"])
+
+	for _, synonym := range apiSynonyms {
+		test.S(t).ExpectTrue(pathsMap[synonym])
+	}
+}
