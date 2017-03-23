91d50cee47fb7b13f930b652d5ca7f633254a5b9
Simon J Mudd <simon.mudd@booking.com>
2017-03-23 22:28:27 +0100

    flushInstanceWriteBuffer: update writeInstanceCounter correctly
    The function makes 2 calls to writeManyInstances but only recorded
    the number of instances affected by the first write. This patch
    corrects that by recording both instances and lastseen.

--- go/inst/instance_dao.go
+++ go/inst/instance_dao.go
@@ -2144,7 +2144,7 @@ func flushInstanceWriteBuffer() {
 			return log.Errorf("flushInstanceWriteBuffer last_seen: %v", err)
 		}
 
-		writeInstanceCounter.Inc(int64(len(instances)))
+		writeInstanceCounter.Inc(int64(len(instances)+len(lastseen)))
 		return nil
 	}
 	err := ExecDBWriteFunc(writeFunc)
