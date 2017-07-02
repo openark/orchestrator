89941894ec7b714052d212b5e6d54afb88ed5c68
Shlomi Noach <shlomi-noach@github.com>
2017-07-02 14:59:06 +0300

    refactored prompt

--- go/app/prompt.go
+++ go/app/prompt.go
@@ -15,27 +15,9 @@ Cheatsheet:
 
     See all possible commands:
 
-        orchestrator -c help
+        orchestrator help
 
     Detailed help for a given command (e.g. "relocate")
 
         orchestrator help relocate
-      or
-        orchestrator -c relocate help
-
-    Usage for most commands:
-        orchestrator -c <command> [-i <instance.fqdn>] [-d <destination.fqdn>] [--verbose|--debug]
-
-    -i (instance):
-        instance on which to operate, in "hostname" or "hostname:port" format.
-        Default port is 3306 (or DefaultInstancePort in config)
-        For some commands this argument can be ommitted altogether, and the
-        value is implicitly the local hostname.
-    -d (Destination)
-        destination instance (used when moving replicas around or when failing over)
-    -s (Sibling/Subinstance/deStination) - synonym to "-d"
-
-    -c (command):
-        Listed below are all available commands; all of which apply for CLI execution (ignored by HTTP mode).
-        Different flags are required for different commands; see specific documentation per commmand.
 `
