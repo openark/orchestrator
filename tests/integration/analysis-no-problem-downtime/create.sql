d04d4d60de1b3ee9af767145570194777fea13d6
Shlomi Noach <shlomi-noach@github.com>
2017-08-24 09:37:44 +0300

    finalizing test

--- tests/integration/analysis-no-problem-downtime/create.sql
+++ tests/integration/analysis-no-problem-downtime/create.sql
@@ -2,5 +2,6 @@ UPDATE database_instance SET slave_sql_running=0 where master_port=22294;
 INSERT INTO database_instance_downtime (
     hostname, port, downtime_active, begin_timestamp, end_timestamp, owner, reason
   ) values (
-    'testhost', 22295, 1, now(), now() + interval 1 minute, 'test', 'integration test'
+    'testhost', 22295, 1, now(), now(), 'test', 'integration test'
   );
+UPDATE database_instance_downtime SET end_timestamp=now() + interval 1 hour;
