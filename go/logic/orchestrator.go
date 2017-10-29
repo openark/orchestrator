64afe5e08acd9826b7d948fe6ef0d4d86da2775f
Shlomi Noach <shlomi-noach@github.com>
2017-10-29 06:17:18 +0200

    better health check validation, improves local test environments

--- go/logic/orchestrator.go
+++ go/logic/orchestrator.go
@@ -285,7 +285,7 @@ func onDiscoveryTick() {
 		} else {
 			atomic.StoreInt64(&isElectedNode, 0)
 		}
-		if process.SinceLastGoodHealthCheck() > time.Minute {
+		if process.SinceLastGoodHealthCheck() > time.Minute && process.SinceLastHealthCheck() < 2*config.DiscoveryPollSeconds*time.Second {
 			orcraft.FatalRaftError(fmt.Errorf("Node is unable to register health. Please check database connnectivity."))
 		}
 	}
