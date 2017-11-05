911523288ecc7b2c26b3acb3e2b74cc787aa6f5f
Shlomi Noach <shlomi-noach@github.com>
2017-11-05 17:16:08 +0200

    support for Consul KV client

--- go/kv/consul.go
+++ go/kv/consul.go
@@ -17,19 +17,58 @@
 package kv
 
 import (
+	"fmt"
+
+	consulapi "github.com/armon/consul-api"
 	"github.com/github/orchestrator/go/config"
 	"github.com/openark/golib/log"
 )
 
+// A Consul store based on config's `ConsulAddress` and `ConsulKVPrefix`
 type consulStore struct {
+	client *consulapi.Client
 }
 
+// NewConsulStore creates a new consul store. It is possible that the client for this store is nil,
+// which is the case if no consul config is provided.
 func NewConsulStore() KVStore {
-	return &consulStore{}
+	store := &consulStore{}
+
+	if config.Config.ConsulAddress != "" {
+		consulConfig := consulapi.DefaultConfig()
+		consulConfig.Address = config.Config.ConsulAddress
+		if client, err := consulapi.NewClient(consulConfig); err != nil {
+			log.Errore(err)
+		} else {
+			store.client = client
+		}
+	}
+	return store
+}
+
+func (this *consulStore) fullKey(key string) string {
+	if config.Config.ConsulKVPrefix != "" {
+		return fmt.Sprintf("%s/%s", config.Config.ConsulKVPrefix, key)
+	}
+	return key
 }
 
 func (this *consulStore) PutKeyValue(key string, value string) (err error) {
+	if this.client == nil {
+		return nil
+	}
+	pair := &consulapi.KVPair{Key: this.fullKey(key), Value: []byte(value)}
+	_, err = this.client.KV().Put(pair, nil)
+	return err
 }
 
 func (this *consulStore) GetKeyValue(key string) (value string, err error) {
+	if this.client == nil {
+		return value, nil
+	}
+	pair, _, err := this.client.KV().Get(this.fullKey(key), nil)
+	if err != nil {
+		return value, err
+	}
+	return string(pair.Value), nil
 }
