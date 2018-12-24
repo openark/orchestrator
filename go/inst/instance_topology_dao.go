302bfc4c11614d544e39bfca656e06f38ddb9d31
Shlomi Noach <shlomi-noach@github.com>
2018-12-24 08:07:34 +0200

    NoReplicationThreadRunning

--- go/inst/instance_topology_dao.go
+++ go/inst/instance_topology_dao.go
@@ -553,7 +553,7 @@ func ChangeMasterCredentials(instanceKey *InstanceKey, masterUser string, master
 		return instance, log.Errorf("Empty user in ChangeMasterCredentials() for %+v", *instanceKey)
 	}
 
-	if instance.ReplicaRunning() {
+	if !instance.NoReplicationThreadRunning() {
 		return instance, fmt.Errorf("ChangeMasterTo: Cannot change master on: %+v because replication is running", *instanceKey)
 	}
 	log.Debugf("ChangeMasterTo: will attempt changing master credentials on %+v", *instanceKey)
