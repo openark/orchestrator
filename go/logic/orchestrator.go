63e4c3ef7681729b13397899a39b5a7caa839ef7
Shlomi Noach <shlomi-noach@github.com>
2019-11-19 11:10:45 +0200

    DiscoveryIgnoreHostnameFilters

--- go/logic/orchestrator.go
+++ go/logic/orchestrator.go
@@ -190,6 +190,11 @@ func DiscoverInstance(instanceKey inst.InstanceKey) {
 		log.Debugf("discoverInstance: skipping discovery of %+v because it is set to be forgotten", instanceKey)
 		return
 	}
+	if inst.RegexpMatchPatterns(instanceKey.StringCode(), config.Config.DiscoveryIgnoreHostnameFilters) {
+		log.Debugf("discoverInstance: skipping discovery of %+v because it matches DiscoveryIgnoreHostnameFilters", instanceKey)
+		return
+	}
+
 	// create stopwatch entries
 	latency := stopwatch.NewNamedStopwatch()
 	latency.AddMany([]string{
