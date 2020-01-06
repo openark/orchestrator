357fbb86e84bc136f53e41f26fb33e3b3b2fcac9
Luis Nuñez <luisyonaldo@gmail.com>
2020-01-06 11:32:33 +0100

    validate InstanceWriteBufferSize

--- go/config/config.go
+++ go/config/config.go
@@ -572,6 +572,9 @@ func (this *Configuration) postReadAdjustments() error {
 		if u.Path != "" {
 			return fmt.Errorf("If specified, HTTPAdvertise must not specify a path")
 		}
+		if this.InstanceWriteBufferSize <= 0 {
+			this.BufferInstanceWrites = false
+		}
 	}
 	return nil
 }
