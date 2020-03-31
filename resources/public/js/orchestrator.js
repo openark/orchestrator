18033ca830a1c6bc07661e89350aeddc0a395888
Maksim Fedotov <m_fedotov@wargaming.net>
2020-04-01 00:19:25 +0300

    orchestrator js fix bug with toHumanFormat func if bytes is 0

--- resources/public/js/orchestrator.js
+++ resources/public/js/orchestrator.js
@@ -117,6 +117,9 @@ function booleanString(b) {
 }
 
 function toHumanFormat(bytes) {
+  if (bytes == 0) {
+    return "0 bytes"
+  }
   var s = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'];
   var e = Math.floor(Math.log(bytes) / Math.log(1024));
   return (bytes / Math.pow(1024, e)).toFixed(2) + " " + s[e];
