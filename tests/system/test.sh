5c8b85ae8e91cd75a5795c493d49a53bdcd464d6
Shlomi Noach <shlomi-noach@users.noreply.github.com>
2020-04-27 10:41:19 +0300

    stderr

--- tests/system/test.sh
+++ tests/system/test.sh
@@ -95,7 +95,7 @@ test_step() {
   fi
   if [ -f $test_path/teardown_redeploy ] ; then
     if [ -f orchestrator-ci-env/script/deploy-replication ] ; then
-      (cd orchestrator-ci-env && script/deploy-replication > $deploy_replication_file && sleep 10)
+      (cd orchestrator-ci-env && script/deploy-replication > $deploy_replication_file 2>&1 && sleep 10)
     else
       echo "ERROR: teardown_redeploy indicated, but orchestrator-ci-env not found
       return 1"
