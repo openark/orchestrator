2b38b1998d3e8cf1dad1a54aeb97380e5d262318
Shlomi Noach <shlomi-noach@users.noreply.github.com>
2020-05-27 11:48:33 +0300

    adapt integration tests

--- tests/integration/analysis-semi-sync-no-valid-replicas/create.sql
+++ tests/integration/analysis-semi-sync-no-valid-replicas/create.sql
@@ -1,2 +1,2 @@
-UPDATE database_instance SET semi_sync_master_enabled=1, semi_sync_master_wait_for_slave_count=1 where port=22293;
-UPDATE database_instance SET semi_sync_replica_enabled=0;
+UPDATE database_instance SET semi_sync_master_enabled=1, semi_sync_master_status=0, semi_sync_master_wait_for_slave_count=1, semi_sync_master_clients=0 where port=22293;
+UPDATE database_instance SET semi_sync_replica_enabled=1;
--- tests/integration/analysis-semi-sync-not-enough-valid-replicas-invalid/create.sql
+++ tests/integration/analysis-semi-sync-not-enough-valid-replicas-invalid/create.sql
@@ -1,3 +1,2 @@
-UPDATE database_instance SET semi_sync_master_enabled=1, semi_sync_master_wait_for_slave_count=2 where port=22293;
-UPDATE database_instance SET semi_sync_replica_enabled=1 where port in (22294, 22296);
+UPDATE database_instance SET semi_sync_master_enabled=1, semi_sync_master_status=0, semi_sync_master_wait_for_slave_count=2, semi_sync_master_clients=1 where port=22293;
 UPDATE database_instance SET last_seen=last_checked - interval 1 minute where port=22296;
