f41a58c31850097db037335f5be018e0c26cc95b
Tim Vaillancourt <timvaillancourt@github.com>
2021-02-04 00:35:33 +0100

    Use official consul client only

--- go/kv/consul.go
+++ go/kv/consul.go
@@ -1,5 +1,5 @@
 /*
-   Copyright 2017 Shlomi Noach, GitHub Inc.
+   Copyright 2021 Shlomi Noach, GitHub Inc.
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -25,7 +25,7 @@ import (
 
 	"github.com/openark/orchestrator/go/config"
 
-	consulapi "github.com/armon/consul-api"
+	consulapi "github.com/hashicorp/consul/api"
 	"github.com/patrickmn/go-cache"
 
 	"github.com/openark/golib/log"
